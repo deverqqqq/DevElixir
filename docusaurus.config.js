667b56f6ace3510025175aaf1ffa7cb18f121cc4
deverqqqq <deverqqqq@outlook.com>
2024-02-10 16:28:19 +0800

    feat.
    1. add custom domain
    2. add CNAME

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,10 +17,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://deverqqqq.github.io',
+  url: 'https://tech.deverqqqq.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/DevElixir/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
